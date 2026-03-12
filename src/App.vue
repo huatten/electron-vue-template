b75b1070cff3292f1d606ddc2d6a338c0a409fe5
滕锦华 <903013546@qq.com>
2026-03-12 14:38:58 +0800

    feat: 引入vue3

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>💖 Hello Electron!</h1>
+</template>
+
+<script setup>
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
--- src/renderer.js
+++ src/renderer.js
@@ -26,7 +26,12 @@
  * ```
  */
 
+
+import { createApp } from 'vue';
+import App from './App.vue';
 import './index.css';
+
+createApp(App).mount('#app');
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via Vite',
