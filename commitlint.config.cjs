92c3f43d4d2cec7851e9c3c8d47311871731c5da
滕锦华 <903013546@qq.com>
2026-03-16 10:47:16 +0800

    chore: 🔨 添加vscode调试

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,37 +1,67 @@
+// @see: https://cz-git.qbb.sh/zh
+const fs = require('fs')
+const path = require('path')
+
+const scopes = fs
+  .readdirSync(path.resolve(__dirname, 'src'), { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name.replace(/s$/, ''))
+
+/** @type {import('cz-git').UserConfig} */
 module.exports = {
+  ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // 类型必须小写
-    'type-case': [2, 'always', 'lower-case'],
-    // 类型必须在以下范围内
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat', // 新功能
-        'fix', // 修复
-        'docs', // 文档变更
-        'style', // 代码格式（不影响代码运行的变动）
-        'refactor', // 重构（既不是新增功能，也不是修复bug的代码变动）
-        'perf', // 性能优化
-        'test', // 测试
-        'build', // 构建系统或外部依赖的变动
-        'ci', // CI配置变动
-        'chore', // 其他修改（不在src或test文件中的修改）
-        'revert', // 回退
-      ],
-    ],
-    // 主题（提交简述）不能为空
+    // @see: https://commitlint.js.org/#/reference-rules
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
     'subject-empty': [2, 'never'],
-    // 主题长度不能超过100个字符
-    'subject-max-length': [2, 'always', 100],
-    // 主题不能以句号结尾
-    'subject-full-stop': [2, 'never', '.'],
-    // 主题必须以空格开头
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    // 正文（body）必须以空行开头
-    'body-leading-blank': [1, 'always'],
-    // 正文每行长度不能超过100个字符
-    'body-max-line-length': [2, 'always', 100],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
   },
-};
+  prompt: {
+    messages: {
+      type: '选择你要提交的类型 :',
+      scope: '选择一个提交范围（可选）:',
+      customScope: '请输入自定义的提交范围 :',
+      subject: '填写简短精炼的变更描述 :\n',
+      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      footerPrefixsSelect: '选择关联issue前缀（可选）:',
+      customFooterPrefixs: '输入自定义issue前缀 :',
+      footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
+      confirmCommit: '是否提交或修改commit ?',
+    },
+    types: [
+      { value: 'feat', name: '特性:   🚀  feat | 新增功能', emoji: '🚀' },
+      { value: 'fix', name: '修复:   🐛  fix | 修复缺陷', emoji: '🐛' },
+      { value: 'docs', name: '文档:   📝  docs | 文档变更', emoji: '📝' },
+      { value: 'style', name: '格式:   💄  style | 代码格式（不影响功能，例如空格、分号等格式修正）', emoji: '💄' },
+      { value: 'refactor', name: '重构:   ♻️  refactor | 代码重构（不包括 bug 修复、功能新增）', emoji: '♻️' },
+      { value: 'perf', name: '性能:   ⚡️  perf | 性能优化', emoji: '⚡️' },
+      { value: 'test', name: '测试:   ✅   test | 添加疏漏测试或已有测试改动', emoji: '✅' },
+      {
+        value: 'build',
+        name: '构建:   🛠  build | 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）',
+        emoji: '🛠',
+      },
+      { value: 'ci', name: '集成:   ⚙️  ci | 修改 CI 配置、脚本', emoji: '⚙️' },
+      { value: 'revert', name: '回退:   ⏪️  revert | 回滚 commit', emoji: '⏪️' },
+      {
+        value: 'chore',
+        name: '其他:   🔨  chore | 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）',
+        emoji: '🔨',
+      },
+    ],
+    useEmoji: true,
+    scopes: [...scopes],
+    customScopesAlign: 'bottom',
+    emptyScopesAlias: '空',
+    customScopesAlias: '自定义',
+    emptyIssuePrefixAlias: '跳过',
+    customIssuePrefixAlias: '自定义',
+    allowBreakingChanges: ['feat', 'fix'],
+  },
+}
